796958b0d8eebb94d3e18e1ae205cd1daf862584
dcmartin <github@dcmartin.com>
2022-06-16 15:55:51 -0700

    lgtm

--- homeassistant/www/favicons/mkfavicon.sh
+++ homeassistant/www/favicons/mkfavicon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-convert ${1:-$(read -p "Image?")} -alpha off -resize 256x256 -define icon:auto-resize="256,128,96,64,48,32,16" favicon.ico
+convert ${1:-$(read -p "Image?")} -alpha on -resize 256x256 -define icon:auto-resize="256,128,96,64,48,32,16" favicon.ico
